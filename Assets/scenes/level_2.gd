d1b463588d9690dc6691080293f2cc0303860a6d
Maxim Gosselin <maximgosselin04@gmail.com>
2026-05-08 18:05:10 -0400

    added a castle, animations, continuation logic,

--- Assets/scenes/level_2.gd
+++ Assets/scenes/level_2.gd
@@ -17,3 +17,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func level_restart():
 	pass
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://Assets/scenes/final-level.tscn")
